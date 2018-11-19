81560d590dab44cef18e08e0418168e0086b0823
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-19 21:52:12 +0200

    wip

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -3,6 +3,7 @@ const jackrabbit = require("jackrabbit");
 const fs = require("fs");
 const path = require("path");
 const AWS = require("aws-sdk");
+const util = require("util");
 
 const rabbit = jackrabbit(config.rabbit.url);
 const exchange = rabbit.default();
@@ -11,31 +12,52 @@ const s3 = new AWS.S3({
     secretAccessKey: config.aws.secretKey
 });
 
+const readFileAsync = util.promisify(fs.readFile);
 
 function upload(msg, ack) {
     const filename = msg;
     const file = path.join(__dirname, "../../", "upload", filename);
 
-    fs.readFile(file, (err, data) => {
-        if (err) {
+    readFileAsync(file)
+        .then((fileBuffer) => {
+            return {
+                Bucket: config.aws.bucket,
+                Key: filename,
+                Body: fileBuffer
+            };
+        })
+        .then((params) => {
+            return s3.putObject(params);
+        })
+        .then((data) => {
+            console.log(`File uploaded successfully at ${data.Location}`)
+            // ack();
+        })
+        .catch((err) => {
             console.log(err);
-            throw err;
-        }
-        const params = {
-            Bucket: config.aws.bucket,
-            Key: filename,
-            Body: file
-        };
-        s3.upload(params, function (s3Err, data) {
-            if (s3Err) {
-                console.log(s3Err);
-                throw s3Err;
-            } else {
-                console.log(`File uploaded successfully at ${data.Location}`)
-                ack();
-            }
         });
-    });
+
+
+    // fs.readFile(file, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //         throw err;
+    //     }
+    //     const params = {
+    //         Bucket: config.aws.bucket,
+    //         Key: filename,
+    //         Body: file
+    //     };
+    //     s3.upload(params, function (s3Err, data) {
+    //         if (s3Err) {
+    //             console.log(s3Err);
+    //             throw s3Err;
+    //         } else {
+    //             console.log(`File uploaded successfully at ${data.Location}`)
+    //             ack();
+    //         }
+    //     });
+    // });
 }
 
 function work() {
