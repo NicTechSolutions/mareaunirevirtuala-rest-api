b3551e36014bd6e5660416223774cc90affea536
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-22 00:49:52 +0200

    remove unused code

--- config/winston.js
+++ config/winston.js
@@ -1,24 +1,6 @@
 const appRoot = require("app-root-path");
 const winston = require("winston");
 
-const enumerateErrorFormat = winston.format(info => {
-    if (info.message instanceof Error) {
-        info.message = Object.assign({
-            message: info.message.message,
-            stack: info.message.stack
-        }, info.message);
-    }
-
-    if (info instanceof Error) {
-        return Object.assign({
-            message: info.message,
-            stack: info.stack
-        }, info);
-    }
-
-    return info;
-});
-
 const options = {
     file: {
         level: "info",
