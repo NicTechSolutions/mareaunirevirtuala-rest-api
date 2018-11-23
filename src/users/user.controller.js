23939aefd9a3cd6690547c7d2033d6a386135200
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-23 15:55:50 +0200

    another romanizare

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -23,7 +23,10 @@ function register(req, res, next) {
             userService.create(req.body)
                 .then((user) => user ? res.json(user) : res.status(400).json())
                 .catch((err) => next(err));
-        }).catch((err) => next(err));
+        }).catch((err) => {
+            err.name = "ValidationErrorRegister";
+            next(err)
+        });
 }
 
 function fbLogin(req, res, next) {
