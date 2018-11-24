9c7e3cb8e3737d5a1d20e0efc819e9ac67e2b537
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-24 15:49:55 +0200

    remove deploy part

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,20 +6,9 @@ module.exports = {
     exec_mode: "cluster",
     autorestart: true,
     watch: false,
-    max_memory_restart: "1G",
+    max_memory_restart: "500M",
     env_production: {
       NODE_ENV: "production"
     }
-  }],
-
-  deploy: {
-    production: {
-      user: "claudiun",
-      host: "68.183.140.93",
-      ref: "origin/master",
-      repo: "https://github.com/NicTechSolutions/nodejs-mongodb-auth.git",
-      path: "/home/claudiun/nodejs-mongodb-auth",
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
