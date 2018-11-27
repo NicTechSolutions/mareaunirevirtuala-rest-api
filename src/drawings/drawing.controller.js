9826ef8dbd09143910214644cdcdb6e130f72a65
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-27 20:48:30 +0200

    get url photo

--- src/drawings/drawing.controller.js
+++ src/drawings/drawing.controller.js
@@ -13,7 +13,14 @@ function upload(req, res, next) {
         .catch((err) => next(err));
 }
 
+function imageUrl(req, res, next) {
+    const userId = req.user.sub;
+
+    res.json(`upload/${userId}.jpg`);
+}
+
 // routes
 router.post("/upload", upload);
+router.get("/", imageUrl);
 
 module.exports = router;
