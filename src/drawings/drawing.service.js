7e09636aa65e29f4a554fc7ea7560744bfc75260
Claudiu Nicola <claudiunicola96@gmail.com>
2018-11-30 22:20:48 +0200

    user upload limit

--- src/drawings/drawing.service.js
+++ src/drawings/drawing.service.js
@@ -3,7 +3,22 @@ const winston = require("../../config/winston");
 const appRoot = require("app-root-path");
 const base64Img = require("base64-img-promise");
 
+const LIMIT = 6;
+
+async function isLimitReached(userId) {
+    const userCount = await userService.getUserDrawingsCount(userId);
+    console.log(userCount);
+    if (userCount > (LIMIT - 1)) {
+        return true;
+    }
+    return false;
+}
+
 async function upload(msgDto) {
+    if (await isLimitReached(msgDto.id) == true) {
+        throw "Ai atinsa limita adminisa de incarcat desene";
+    }
+
     if (msgDto.data) {
         const path = `${appRoot}/storage/upload`;
         return Promise.all([
